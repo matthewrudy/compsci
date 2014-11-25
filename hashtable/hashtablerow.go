b6fbaa2b36a25976df1a1ee4357b9cf3f856b314
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2014-11-25 12:38:16 +0800

    multiple entries into the hashtable
    use a linked list

--- hashtable/hashtablerow.go
+++ hashtable/hashtablerow.go
@@ -3,11 +3,24 @@ package hashtable
 type hashTableRow struct {
 	key   string
 	value interface{}
+	next  *hashTableRow
+}
+
+func (self *hashTableRow) find(key string) (row *hashTableRow) {
+	if self.key == key {
+		return self
+	} else if self.next != nil {
+		return self.next.find(key)
+	} else {
+		return nil
+	}
 }
 
 func (self *hashTableRow) Get(key string) (value interface{}) {
-	if self.key == key {
-		return self.value
+	row := self.find(key)
+
+	if row != nil {
+		return row.value
 	} else {
 		return nil
 	}
@@ -19,16 +32,20 @@ func (self *hashTableRow) Set(key string, value interface{}) (row *hashTableRow,
 			key:   key,
 			value: value,
 		}, true
+	}
+
+	entry := self.find(key)
+
+	if entry == nil {
+		return &hashTableRow{
+			key:   key,
+			value: value,
+			next:  self,
+		}, true
 
 	} else {
-		if self.key != key {
-			added = true
-		} else {
-			added = false
-		}
+		entry.value = value
 
-		self.value = value
-
-		return self, added
+		return self, false
 	}
 }
