a53d7de0f60fd9ec9edfbb8d939f6a42538a7aac
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2014-11-27 11:55:37 +0800

    an attempt at "journey to the moon"

---
+++ hackerrank/journeytothemoon.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func parseInt(line string) (parsed int) {
+	parsed, _ = strconv.Atoi(line)
+	return parsed
+}
+
+func readLine(reader *bufio.Reader) (line string) {
+	line, _ = reader.ReadString('\n')
+	return strings.TrimSpace(line)
+}
+
+func readInt(reader *bufio.Reader) (read int) {
+	line := readLine(reader)
+	return parseInt(line)
+}
+
+func writeLine(writer *bufio.Writer, line string) {
+	writer.WriteString(line)
+	writer.WriteString("\n")
+}
+
+func writeInt(writer *bufio.Writer, number int) {
+	line := strconv.Itoa(number)
+	writeLine(writer, line)
+}
+
+func readInts(reader *bufio.Reader) []int {
+	line := readLine(reader)
+	chars := strings.Split(line, " ")
+
+	ints := make([]int, len(chars))
+	for i, n := range chars {
+		ints[i] = parseInt(n)
+	}
+	return ints
+}
+
+func readIntPair(reader *bufio.Reader) (a int, b int) {
+	ints := readInts(reader)
+	a = ints[0]
+	b = ints[1]
+
+	return
+}
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	n, count := readIntPair(reader)
+
+	countries := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		countries[i] = i
+	}
+
+	for i := 0; i < count; i++ {
+		a, b := readIntPair(reader)
+
+		aCountry := countries[a]
+		bCountry := countries[b]
+
+		for j := 0; j < n; j++ {
+			if countries[j] == bCountry {
+				countries[j] = aCountry
+			}
+		}
+	}
+
+	countryCounts := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		country := countries[i]
+		countryCounts[country] += 1
+	}
+
+	result := 1
+
+	for i := 0; i < n; i++ {
+		if countryCounts[i] > 0 {
+			result *= countryCounts[i]
+		}
+	}
+
+	writeInt(writer, result)
+}
